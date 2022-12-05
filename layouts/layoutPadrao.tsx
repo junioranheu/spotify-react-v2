134c868390106a6a46b7990fd6c0c5aa493ac378
junioranheu <junioranheu@gmail.com>
2022-12-05 20:02:26 -0300

    ajustes gerais

--- layouts/layoutPadrao.tsx
+++ layouts/layoutPadrao.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useContext, useEffect, useState } from 'react';
 import NavbarPadrao from '../components/navbar/navbar-padrao/navbar-padrao';
 import Sidebar from '../components/navbar/sidebar/sidebar';
 import Barra from '../components/player/barra';
@@ -8,6 +9,8 @@ import Styles from './styles/layoutPadrao.module.scss';
 
 export default function LayoutPadrao({ Component, pageProps }: any) {
 
+    const { asPath } = useRouter();
+
     const usuarioContext = useContext(UsuarioContext); // Contexto do usuário;
     const [isAuth, setIsAuth] = [usuarioContext?.isAuthContext[0], usuarioContext?.isAuthContext[1]];
 
@@ -16,6 +19,16 @@ export default function LayoutPadrao({ Component, pageProps }: any) {
         verificarTokenValido(isAuth);
     }, [isAuth]);
 
+    // Renovar animação a cada mudança de URL (router.asPath);
+    const [efeitoAnimar, setEfeitoAnimar] = useState('');
+    useEffect(() => {
+        setEfeitoAnimar('animate__animated animate__fadeIn animate__delay03');
+
+        setTimeout(function () {
+            setEfeitoAnimar('');
+        }, 1000);
+    }, [asPath]);
+
     return (
         <section className='main semHighlight'>
             <section className={Styles.wrapper}>
@@ -23,7 +36,7 @@ export default function LayoutPadrao({ Component, pageProps }: any) {
                     <Sidebar />
                 </section>
 
-                <section className={Styles.div100}>
+                <section className={`${efeitoAnimar} ${Styles.div100}`}>
                     <NavbarPadrao />
                     <Component {...pageProps} />
                 </section>
