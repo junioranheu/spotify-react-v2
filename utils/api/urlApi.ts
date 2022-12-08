ccbefb9928204f876fc9825efcb373b01a756fc6
junioranheu <junioranheu@gmail.com>
2022-12-07 21:47:05 -0300

    ajustes gerais scss

--- utils/api/urlApi.ts
+++ utils/api/urlApi.ts
@@ -2,7 +2,7 @@ let API_BASE_URL_DEV = 'https://localhost:7225';
 let API_BASE_URL_PROD = 'https://spotifyapi.azurewebsites.net';
 
 // Habilitar dev = prod;
-// API_BASE_URL_DEV = API_BASE_URL_PROD;
+API_BASE_URL_DEV = API_BASE_URL_PROD;
 
 export { API_BASE_URL_DEV, API_BASE_URL_PROD };
 
