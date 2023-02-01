1a4173e43dad1b155a04050dd5c8ddcdf6dd7443
junioranheu <junioranheu@gmail.com>
2023-01-31 21:11:57 -0300

    forçar update

--- utils/api/urlApi.ts
+++ utils/api/urlApi.ts
@@ -3,6 +3,6 @@ let API_BASE_URL_PROD = 'https://spotifyapi.azurewebsites.net';
 
 // Habilitar dev = prod;
 API_BASE_URL_DEV = API_BASE_URL_PROD;
-
+ 
 export { API_BASE_URL_DEV, API_BASE_URL_PROD };
 
