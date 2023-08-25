a145c61e99201c211db267b312a77b0f22c9f39d
junioranheu <junioranheu@gmail.com>
2023-08-25 14:37:50 -0300

    fix warnings

--- pages/playlist/gerenciar/[id]/[nome].tsx
+++ pages/playlist/gerenciar/[id]/[nome].tsx
@@ -25,6 +25,16 @@ export default function Playlist() {
     const { id } = router.query;
     const usuarioId = Auth?.get()?.usuarioId ?? 0;
 
+    const refBtn = useRef<HTMLButtonElement | any>(null);
+
+    const [arquivoUploadCapa, setArquivoUploadCapa] = useState<string | null>(''); // ('/1.webp');
+    const [formData, setFormData] = useState<iFormDataPlaylist>({
+        nome: '',
+        sobre: '',
+        foto: arquivoUploadCapa,
+        corDominante: ''
+    });
+
     const [playlist, setPlaylist] = useState<iPlaylist>();
     useEffect(() => {
         async function getPlaylist(playlistId: string) {
@@ -63,17 +73,7 @@ export default function Playlist() {
         if (id) {
             getPlaylist(id.toString());
         }
-    }, [id, usuarioId]);
-
-    const refBtn = useRef<HTMLButtonElement | any>(null);
-
-    const [arquivoUploadCapa, setArquivoUploadCapa] = useState<string | null>(''); // ('/1.webp');
-    const [formData, setFormData] = useState<iFormDataPlaylist>({
-        nome: '',
-        sobre: '',
-        foto: arquivoUploadCapa,
-        corDominante: ''
-    });
+    }, [id, usuarioId, formData]);
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         setFormData({ ...formData, [e?.target?.name]: e?.target?.value });
