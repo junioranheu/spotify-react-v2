c51f2d5245d6bea1c611d78502ae39f017fe0fe5
junioranheu <junioranheu@gmail.com>
2024-03-23 19:51:05 -0300

    🛠️ npm i react-cookie-gpdr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import 'nprogress/nprogress.css';
 import { Fragment, useEffect, useState } from 'react';
+import { CookieWidget } from 'react-cookie-gpdr';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import LayoutPadrao from '../layouts/layoutPadrao';
@@ -56,6 +57,25 @@ export default function App({ Component, pageProps }: AppProps) {
 
                         {/* Conteúdo */}
                         {verificarLayout()}
+
+                        {/* Cookies */}
+                        <section>
+                            <CookieWidget
+                                location='right'
+                                color='var(--cor-principal)'
+                                policyLink=''
+                                policyLinkText=''
+                                title={CONSTS_SISTEMA.NOME_SISTEMA}
+                                subtitle='Um clone do Spotify (2022), feito com React.js e Next.js, desenvolvido por @junioranheu. Este projeto não é de forma alguma afiliado ao Spotify e não deve ser confundido com o produto real.'
+                                text={`O ${CONSTS_SISTEMA.NOME_SISTEMA} usa cookies para oferecer uma experiência melhor. Ao continuar navegando, você concorda com o uso de cookies.`}
+                                cookieSecurity={true}
+                                hideOnScrollDown={false}
+                                rejectButtonText='Rejeitar'
+                                acceptButtonText='Aceitar cookies'
+                                onAccept={() => null}
+                                onReject={() => null}
+                            />
+                        </section>
 
                         {/* Elemento para os modais */}
                         <div id='modalWrapper'></div>
--- utils/consts/outros/sistema.ts
+++ utils/consts/outros/sistema.ts
@@ -1,7 +1,7 @@
 const CONSTS_SISTEMA = {
     NOME_SISTEMA: '@SpotifyAnheu',
     SLOGAN: '',
-    DESCRICAO_SISTEMA: ''
+    DESCRICAO_SISTEMA: 'Clone do Spotify (2022) feito por @junioranheu'
 };
 
 export default CONSTS_SISTEMA;
